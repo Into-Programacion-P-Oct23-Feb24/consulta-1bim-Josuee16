4874dfcc569316cd1242ee3f4091d3c97d267406
Josuee16 <147664232+Josuee16@users.noreply.github.com>
2023-12-01 20:59:34 -0500

    Add files via upload

---
+++ consulta001/Ejemplo1.py
@@ -0,0 +1,6 @@
+num1= int(input("Ingrese el valor entero 1\n" ))
+num2= int(input("Ingrese el valor entero 2\n" ))
+
+resultadosuma = num1 + num2
+
+print("La suma de los enteros es: ",resultadosuma)
---
+++ consulta001/Ejemplo2.py
@@ -0,0 +1,3 @@
+nombre = (input("Ingrese su nombre\n"))
+apellido = (input("Ingrese su apellido\n"))
+print("Su nombre y tu apellido es:",nombre,apellido)
