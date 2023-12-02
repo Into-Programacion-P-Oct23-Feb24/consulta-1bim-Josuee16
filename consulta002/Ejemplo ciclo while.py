19a7ac5d5eb443659cf40e0a1c409b67e936062e
Josuee16 <147664232+Josuee16@users.noreply.github.com>
2023-12-01 21:36:58 -0500

    Add files via upload

---
+++ consulta002/Ejemplo ciclo while.py
@@ -0,0 +1,7 @@
+num = 20
+contador = 1
+suma = 0
+while contador <= num:
+    suma = suma + num
+    print(suma)
+    contador = contador + 1
---
+++ consulta002/Ejemplo condicionales.py
@@ -0,0 +1,7 @@
+num = int(input(("Ingrese el numero\n")))
+if num > 30:
+    print("El numero ingresaso es mayor a 30:",num)
+elif num ==15:
+    print("El numero ingresado es 15")    
+else:
+    print("El numero ingresado es menor a 30:",num)    
